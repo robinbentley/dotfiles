94d35cceeba6f0f15d1f11c0db2ab9ffa536837a
Robin Bentley <robinbentley@me.com>
2015-09-20 15:09:16 +0100

    Bring in updates from nix-dotfiles

---
+++ zsh/aliases.zsh
@@ -0,0 +1,26 @@
+# general
+alias ll='ls -la --color=auto'
+alias tree='tree -L 2'
+alias cl='clear'
+
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ~='cd ~'
+
+# grep
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# network
+alias ping='ping -c 5'
+
+# django manage commands
+alias djsyncdb="python manage.py syncdb"
+alias djmake="python manage.py makemigrations"
+alias djserve="python manage.py runserver"
+alias djmigrate="python manage.py migrate"
+alias djapp="python manage.py startapp"
+alias djproject="python manage.py startproject"
+alias djtest="python manage.py test"
