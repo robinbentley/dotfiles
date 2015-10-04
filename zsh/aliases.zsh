135abb461b3e45b7f07484c6f3a749cfb6d840d9
Robin Bentley <robinbentley@me.com>
2015-10-04 19:01:40 +0100

    Commands to show/hide hidden files in osx

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -30,3 +30,7 @@ alias djmigrate="python manage.py migrate"
 alias djapp="python manage.py startapp"
 alias djproject="python manage.py startproject"
 alias djtest="python manage.py test"
+
+# toggling hidden files in OS X
+alias showhidden="defaults write com.apple.finder AppleShowAllFiles -bool YES && killall Finder"
+alias hidehidden="defaults write com.apple.finder AppleShowAllFiles -bool NO  && killall Finder"
