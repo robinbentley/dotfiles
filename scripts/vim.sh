d57a50a00b6deb6c8d00f359a5dde38be0bd859a
Robin Bentley <robinbentley@me.com>
2016-09-28 15:23:52 +0100

    Add check to see if vim-plug is already installed

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,11 +2,14 @@
 mkdir ~/.vim/swap
 
 # get and install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -e "$HOME/.vim/autoload/plug.vim" ]; then
+  echo "vim-plug is already installed..."
+else
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # link up vimrc
 ln -sfv $HOME/dotfiles/vimrc $HOME/.vimrc
 
-# install plugins from cli
+# install plugins
 vim +PlugInstall +qall
