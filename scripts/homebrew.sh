6cac9c9898e2f02240ea929179ad49df6b35aabb
Robin <robinbentley@users.noreply.github.com>
2016-09-28 15:28:20 +0100

    Formatting

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -22,7 +22,7 @@ which -s brew
 if [[ $? != 0 ]] ; then
   yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-  echo "Homebrew already installed ..."
+  echo "Homebrew already installed..."
 fi
 
 echo "Updating homebrew..."
