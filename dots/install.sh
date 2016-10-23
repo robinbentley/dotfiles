ac62753e754e9450b7fbef91e415e8013b9c1501
Robin Bentley <robinbentley@me.com>
2016-10-23 15:53:15 +0100

    Fix typo in gitconfig symlink

--- dots/install.sh
+++ dots/install.sh
@@ -1,5 +1,5 @@
 echo "Linking dots...."
 
 ln -sfv ~/dotfiles/dots/hushlogin ~/.hushlogin
-ln -sfv ~/dotfiles/dots/giconfig ~/.gitconfig
+ln -sfv ~/dotfiles/dots/gitconfig ~/.gitconfig
 ln -sfv ~/dotfiles/dots/gemrc ~/.gemrc
