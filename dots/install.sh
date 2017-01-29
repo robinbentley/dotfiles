c2b3d079c5dfa1f5ed8dcd62fa291f729fd391ba
Robin Bentley <robinbentley@me.com>
2017-01-29 13:41:52 +0000

    fix for neofetch shell version bug

--- dots/install.sh
+++ dots/install.sh
@@ -4,4 +4,4 @@ ln -sfv ~/dotfiles/dots/hushlogin ~/.hushlogin
 ln -sfv ~/dotfiles/dots/gitconfig ~/.gitconfig
 ln -sfv ~/dotfiles/dots/gemrc ~/.gemrc
 ln -sfv ~/dotfiles/dots/htoprc ~/.config/htop/htoprc
-ln -sfv ~/dotfiles/dots/neofetchrc ~/.neofetchrc
+ln -sfv ~/dotfiles/dots/neofetchrc ~/.config/neofetch/neofetchrc
