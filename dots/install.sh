ee02e4290d5ed5b7c0324a3a394ecb7a0eef69b7
Robin Bentley <robinbentley@me.com>
2017-06-21 16:32:23 +0100

    Add global gitignore to repo

--- dots/install.sh
+++ dots/install.sh
@@ -3,6 +3,7 @@ echo "Linking dots...."
 ln -sfv ~/dotfiles/dots/hushlogin ~/.hushlogin
 ln -sfv ~/dotfiles/dots/gitconfig ~/.gitconfig
 ln -sfv ~/dotfiles/dots/gemrc ~/.gemrc
+ln -sfv ~/dotfiles/dots/gitignore.global ~/.gitignore.global
 
 mkdir ~/.config/htop/
 ln -sfv ~/dotfiles/dots/htoprc ~/.config/htop/htoprc
