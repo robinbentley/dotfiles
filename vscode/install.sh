58fa73d78e2b79c845cc88f3623e597a6b6d4276
Robin Bentley <robinbentley@me.com>
2017-11-25 13:11:26 +0000

    Add VSCode snippets

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,2 +1,3 @@
 mkdir -p ~/Library/Application\ Support/Code/User
 ln -sfv $(pwd)/vscode/*.json ~/Library/Application\ Support/Code/User
+ln -sfv $(pwd)/vscode/snippets/ ~/Library/Application\ Support/Code/User
