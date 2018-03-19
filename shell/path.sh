ab6fc6d83651fdcda0c541d510f2fd9864c92c02
Robin Bentley <robinbentley@me.com>
2018-03-19 23:59:33 +0000

    Move to pipenv

--- shell/path.sh
+++ shell/path.sh
@@ -16,18 +16,9 @@ export NVM_DIR="$HOME/.nvm"
   . "/usr/local/opt/nvm/nvm.sh"
 
 # =====================================================
-# make virtualenv easier with virtualenvwrapper
+# keep virtualenvs in one place
 # =====================================================
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
-export PIP_RESPECT_VIRTUALENV=true
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-else
-    echo "WARNING: Can't find virtualenvwrapper.sh"
-fi
 
 # =====================================================
 # Add brew /bin and /sbin to path
