dba4da4aeb2784f3ab676c7c979bacdc7d5575aa
Robin Bentley <robinbentley@me.com>
2019-01-22 10:07:29 +0000

    Updates

--- shell/path.sh
+++ shell/path.sh
@@ -38,3 +38,8 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
+
+# =====================================================
+# add brew ruby to path
+# =====================================================
+export PATH="/usr/local/opt/ruby/bin:$PATH"
